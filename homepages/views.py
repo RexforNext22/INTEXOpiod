1df94052f3290272498ba6ef4ef1d2aeb114571d
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2021-12-01 11:47:29 -0700

    added by ryan

--- homepages/views.py
+++ homepages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext
 
 # Create your views here.
+
+# Index page function/views
+def indexPageView(request) :
+    return render(request, 'homepages/index.html')
