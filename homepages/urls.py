ff595277aecf5a04e33accdf2307f685d8b72673
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2021-12-01 19:08:50 -0700

    added by ryan

--- homepages/urls.py
+++ homepages/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from .views import indexPageView
 from .views import drugLibraryPageView, prescribersPageView, drugDetailsPageView
-from .views import viewPrescriberPageView, filterPrescriberPageView
+from .views import viewPrescriberPageView, filterPrescriberPageView, addPrescriberPageView
 
 
 # Create the different path for index and about 
@@ -12,6 +12,7 @@ urlpatterns = [
     path("prescribers/", prescribersPageView, name="prescribers"), # route for the prescriber library
     path("showdrugs/<int:drug_id>", drugDetailsPageView, name='showDrugs'), # route for the drug details
     path("viewprescribers/<int:prescriber_id>", viewPrescriberPageView, name="viewPrescriber"), # route to view the prescriber details
-    path("filterprescriber/", filterPrescriberPageView, name="filterprescriber"), # route for the filter prescriber details      
+    path("filterprescriber/", filterPrescriberPageView, name="filterprescriber"), # route for the filter prescriber details 
+    path("addPrescriber/", addPrescriberPageView, name="addPrescriber"), # route for the filter prescriber details           
 ]
 
